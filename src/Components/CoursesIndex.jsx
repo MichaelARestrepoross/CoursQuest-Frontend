0072b04dfd5ac8c984f6a1e41dff78401927bed7
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2024-03-24 16:28:28 -0400

    formatted cards for all courses view/ course index view

--- src/Components/CoursesIndex.jsx
+++ src/Components/CoursesIndex.jsx
@@ -1,7 +1,24 @@
 import { Link } from "react-router-dom";
+import SingleCourse from "./SingleCourse";
 
-const CoursesIndex = () => {
-  return <div>CoursesIndex</div>;
+const CoursesIndex = ({ courses }) => {
+  console.log(courses);
+  return (
+    <>
+      <div className="mx-16 py-12">
+        <div className="text-5xl pb-12"></div>
+        <div className="text-4xl font-extrabold">Courses</div>
+        <hr className="mt-1 mb-6 border-2" />
+        <div className="">
+          <div className="grid sm:grid-cols-1 md:grid-cols-3 gap-6">
+            {courses.map((course) => (
+              <SingleCourse key={course.id} course={course} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default CoursesIndex;
