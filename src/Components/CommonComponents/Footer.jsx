e1a19615db0d5be11932fa0feb63ee8bf8022bb8
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2024-03-25 00:58:06 -0400

    imported react icons, formatted footer and linked proper pages and repo link

--- src/Components/CommonComponents/Footer.jsx
+++ src/Components/CommonComponents/Footer.jsx
@@ -1,7 +1,34 @@
+import { Link } from "react-router-dom";
+import { Copyright } from "lucide-react";
+import { BsGithub } from "react-icons/bs";
+
 const Footer = () => {
   return (
     <div>
-      <div className="bg-blue-400">Footer</div>
+      <div className="bg-slate-500 px-4">
+        <div className="text-white py-10 flex justify-center">
+          <Link to={"/aboutthedevs"}>
+            <div className="hover:text-blue-200">About</div>
+          </Link>
+          <Link
+            to={"https://github.com/MichaelARestrepoross/CoursQuest-Backend"}
+            target="_blank"
+          >
+            <BsGithub className=" w-5 h-5 mt-1 ml-10 lg:ml-48 hover:text-purple-200" />
+          </Link>
+        </div>
+        <div className="flex flex-row justify-between">
+          <Link to={"/"}>
+            <div className="font-bold text-2xl text-white inline-block pb-8 pr-10">
+              CQ
+            </div>
+          </Link>
+          <span className="flex flex-row pt-3 pb-9 text-white text-sm">
+            <Copyright className="w-5 h-5 pr-1" />
+            2024 CourseQuest Inc.
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
