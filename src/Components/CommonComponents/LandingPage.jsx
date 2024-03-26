aca5606c8c2429dadb24be9643b4a42f187f5671
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2024-03-26 16:40:53 -0400

    added hero section to landing page and routed call to action button to courseindex view

--- src/Components/CommonComponents/LandingPage.jsx
+++ src/Components/CommonComponents/LandingPage.jsx
@@ -1,19 +1,41 @@
+import { useNavigate } from "react-router-dom";
+
 const LandingPage = () => {
+  const navigate = useNavigate();
   return (
-    <div>
+    <div className="">
       <div>
-        <div className="relative">
-          <img
-            src="https://res.cloudinary.com/dvmczcg3f/image/upload/v1711480893/CourseQuest%20-%20App/students_learning-jpg_y4hd2r.jpg"
-            alt="img of students"
-            className="w-full opacity-90"
-          />
-          <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center">
-            <div className="text-white text-5xl z-10">
-              Education that moves with you...
+        <section class="bg-gray-800 text-white py-20 px-5 md:px-20">
+          <div class="container mx-auto">
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-10 items-center">
+              <div>
+                <h1 class="text-4xl md:text-6xl font-bold mb-5">
+                  Welcome to CourseQuest
+                </h1>
+                <p class="text-lg md:text-xl mb-8">
+                  Empower your learning journey with CourseQuest, where you can
+                  access a vast array of courses anytime, anywhere. Whether
+                  you're a student seeking academic excellence or an enthusiast
+                  exploring new hobbies, CourseQuest has something for everyone.
+                </p>
+                <button
+                  href="#"
+                  class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-3 px-6 rounded inline-block"
+                  onClick={() => navigate("/courses")}
+                >
+                  Get Started
+                </button>
+              </div>
+              <div>
+                <img
+                  src="https://res.cloudinary.com/dvmczcg3f/image/upload/v1711480893/CourseQuest%20-%20App/students_learning-jpg_y4hd2r.jpg"
+                  alt="Students Learning"
+                  class="w-full rounded shadow-xl"
+                />
+              </div>
             </div>
           </div>
-        </div>
+        </section>
       </div>
     </div>
   );
