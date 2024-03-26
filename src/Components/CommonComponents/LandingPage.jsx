651b2d0864e81865cae3bb34366aa85df08ed68a
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2024-03-26 15:53:14 -0400

    landing patch

--- src/Components/CommonComponents/LandingPage.jsx
+++ src/Components/CommonComponents/LandingPage.jsx
@@ -2,14 +2,14 @@ const LandingPage = () => {
   return (
     <div>
       <div>
-        <div class="relative">
+        <div className="relative">
           <img
             src="https://res.cloudinary.com/dvmczcg3f/image/upload/v1711480893/CourseQuest%20-%20App/students_learning-jpg_y4hd2r.jpg"
             alt="img of students"
-            class="w-full opacity-90"
+            className="w-full opacity-90"
           />
-          <div class="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center">
-            <div class="text-white text-5xl z-10">
+          <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center">
+            <div className="text-white text-5xl z-10">
               Education that moves with you...
             </div>
           </div>
