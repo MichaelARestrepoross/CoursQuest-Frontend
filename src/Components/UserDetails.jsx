6c379e002385039ecfdd36e685d3c3b180ba5455
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2024-03-26 10:18:08 -0400

    styled about the devs section, changed single course card banners

--- src/Components/UserDetails.jsx
+++ src/Components/UserDetails.jsx
@@ -1,32 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import SingleCourse from './SingleCourse';
-import App from '../App';
-const UserDetails = ({API}) => {
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import SingleCourse from "./SingleCourse";
+import App from "../App";
+const UserDetails = ({ API }) => {
   // const { id } = useParams();
 
   const [user, setUser] = useState(null);
   const URL = `${API}/api/users/1`;
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  function navigateCourse(Id){
+  function navigateCourse(Id) {
     navigate(`/courses/${Id}`);
   }
 
   useEffect(() => {
     fetch(URL)
-      .then(response => response.json())
-      .then(data => {
-        setUser(data); 
+      .then((response) => response.json())
+      .then((data) => {
+        setUser(data);
       })
-      .catch(error => console.error('Error fetching user details:', error));
-  }, []); 
+      .catch((error) => console.error("Error fetching user details:", error));
+  }, []);
 
   return (
     <div className="container mx-auto px-4">
       {user && (
-        <div className='mb-6'>
-          <h2 className="text-3xl font-bold mb-8">User Account</h2>
+        <div className="mb-6">
+          <h2 className="text-4xl mb-8 pt-24 font-extrabold">User Account</h2>
           <div className="bg-gray-200 rounded-lg p-6 mb-8">
             <p className="text-lg">
               <span className="font-bold">Name:</span> {user.name}
@@ -35,14 +35,14 @@ const UserDetails = ({API}) => {
               <span className="font-bold">Email:</span> {user.email}
             </p>
             <p className="text-lg">
-              <span className="font-bold">Is Faculty:</span>{' '}
+              <span className="font-bold">Is Faculty:</span>{" "}
               {user.is_faculty ? <span>&#10003;</span> : <span>&#10060;</span>}
             </p>
           </div>
-          <h3 className="text-2xl font-bold mb-4">My Courses</h3>
+          <h3 className="text-3xl font-bold mb-4">My Courses</h3>
           <div className="border-b-2 border-gray-300 mb-6"></div>
           <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-            {user.filteredCourses.map(course => (
+            {user.filteredCourses.map((course) => (
               <SingleCourse key={course.id} course={course} />
             ))}
           </div>
