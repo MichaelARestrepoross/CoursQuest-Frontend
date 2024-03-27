3f95935bb4edb6bf7822e4c9d3c2a3507e0f619c
Jennifer <jenniferpeterson@pursuit.org>
2024-03-27 17:55:43 -0400

    fixed the position of the footer to the bottom of the page and realigned footer divs and took away some padding

--- src/Components/CommonComponents/Footer.jsx
+++ src/Components/CommonComponents/Footer.jsx
@@ -4,32 +4,32 @@ import { BsGithub } from "react-icons/bs";
 
 const Footer = () => {
   return (
-    <div>
-      <div className="bg-slate-500 px-4">
-        <div className="text-white py-10 flex justify-center">
-          <Link to={"/aboutthedevs"}>
-            <div className="hover:text-blue-200">About</div>
-          </Link>
-          <Link
-            to={"https://github.com/MichaelARestrepoross/CoursQuest-Backend"}
-            target="_blank"
-          >
-            <BsGithub className=" w-5 h-5 mt-1 ml-10 lg:ml-48 hover:text-purple-200" />
-          </Link>
-        </div>
+    <footer>
+      <div className="bg-slate-500 px-4 fixed bottom-0 left-0 right-0">
         <div className="flex flex-row justify-between">
           <Link to={"/"}>
-            <div className="font-bold text-2xl text-white inline-block pb-8 pr-10">
+            <div className="lg:pr-16 font-bold text-2xl text-white inline-block pt-3 pr-10">
               CQ
             </div>
           </Link>
-          <span className="flex flex-row pt-3 pb-9 text-white text-sm">
-            <Copyright className="w-5 h-5 pr-1" />
-            2024 CourseQuest Inc.
+          <div className="text-white flex flex-row  pt-5 ">
+            <Link to={"/aboutthedevs"}>
+              <div className="hover:text-blue-400 lg:mr-10 ">About</div>
+            </Link>
+            <Link
+              to={"https://github.com/MichaelARestrepoross/CoursQuest-Backend"}
+              target="_blank"
+            >
+              <BsGithub className="lg:mr-16 mr-3 w-5 h-5 mt-1 ml-10 lg:ml-48 hover:text-purple-400" />
+            </Link>
+          </div>
+          <span className="flex flex-row pt-6 pb-4 text-white text-sm">
+            {/* <Copyright className="w-5 h-5 pr-1 pt-5" /> */}
+            &#169;2024 CourseQuest Inc.
           </span>
         </div>
       </div>
-    </div>
+    </footer>
   );
 };
 
